2547af6a94c80df03c25376c72992e7e70a08801
Jose Maria Gonzalez Lopez <josegonzalezlopez@gmail.com>
2020-08-10 08:00:59 -0300

    Update GifGrid.js

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -9,7 +9,7 @@ const GifGrid = ({category})=>{
 
     return(
         <Fragment>
-            <h3 className="card animate__animated animate__fadeIn">{ category }</h3>
+            <h3 className="animate__animated animate__fadeIn">{ category }</h3>
             { loading && <p className="card animate__animated animate__flash">Cargando...</p> }
              <div className="card-grid">
                 {
